6484f46b5d6f76934f822aac854383bc636c1049
Feather Tin <x6495ED@gmail.com>
2019-05-05 10:11:38 +0100

    Nikola auto commit.
    Nikola version: 8.0.2

--- conf.py
+++ conf.py
@@ -967,7 +967,7 @@ COMMENT_SYSTEM = "disqus"
 # depends on what comment system you use. The default is
 # "nikolademo" which is a test account for Disqus. More information
 # is in the manual.
-COMMENT_SYSTEM_ID = "feathertin"
+COMMENT_SYSTEM_ID = "the-tin-feather.disqus.com"
 
 # Create index.html for page folders?
 # WARNING: if a page would conflict with the index file (usually
